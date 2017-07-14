e909d23669e47445142300469bd607f3bd2edc6a
cjl <cuijialiang@pku.edu.cn>
2017-07-14 18:06:10 +0800

    runet class write OK

--- tf_unet/runet.py
+++ tf_unet/runet.py
@@ -34,13 +34,15 @@ from tf_unet.layers import (weight_variable, weight_variable_devonc, bias_variab
 
 from tensorflow.contrib import rnn
 
+from crnn import block_c_rnn
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
-def create_conv_r_net_nobatch(x, keep_prob, channels, n_class, layers=3, features_root=16, filter_size=3, pool_size=2, summaries=True):
+def create_conv_r_net_test(x, keep_prob, channels, n_class, layers=3, features_root=16, filter_size=3, pool_size=2, summaries=True):
     """
     Creates a new convolutional unet for the given parametrization.
 
-    :param x: input tensor, shape [?,nx,ny,channels]
+    :param x: input tensor, shape [?(batch_size),?(steps),nx,ny,channels]
     #:param gt1: input tensor, ground truth of the first frame, shape [1,nx,ny,n_class]
     :param keep_prob: dropout probability tensor
     :param channels: number of channels in the input image
@@ -57,133 +59,33 @@ def create_conv_r_net_nobatch(x, keep_prob, channels, n_class, layers=3, feature
                                                                                                                                             filter_size=filter_size,
                                                                                                                                             pool_size=pool_size))
     # Placeholder for the input image
-    nx = tf.shape(x)[1]
-    ny = tf.shape(x)[2]
-    x_image = tf.reshape(x, tf.stack([-1, nx, ny, channels]))
-    #x_image = block_rnn_for_gt1(x_image, gt1)
-    in_node = x_image # size as [step_size, nx, ny, channels]
-    batch_size = tf.shape(x_image)[0]
-
-    weights = []
-    biases = []
-    convs = []
-    pools = OrderedDict()
-    deconv = OrderedDict()
-    dw_h_convs = OrderedDict()
-    up_h_convs = OrderedDict()
+    batch_size = tf.shape(x)[0]
+    steps = tf.shape(x)[1]
+    nx = tf.shape(x)[2]
+    ny = tf.shape(x)[3]
+    x_image = tf.reshape(x, tf.stack([batch_size, steps, nx, ny, channels]))
+    in_node = x_image # size as [batch_size, step_size, nx, ny, channels]
 
     in_size = 1000
     size = in_size
-    # down layers
-    for layer in range(0, layers):
-        features = 2**layer * features_root
-        stddev = np.sqrt(2 / (filter_size**2 * features))
-        if layer == 0:
-            w1 = weight_variable(
-                [filter_size, filter_size, channels + n_class, features], stddev)
-        else:
-            w1 = weight_variable(
-                [filter_size, filter_size, features // 2, features], stddev)
+    with tf.variable_scope('r_net_test') as vs:
+        initstate = tf.placeholder(dtype=tf.float32, shape=[batch_size,nx,ny,channels])
+        in_node = block_c_rnn(in_node,initstate)
+    
+    with tf.variable_scope('conv1') as vs:
+        stddev = np.sqrt(2 / (filter_size**2 * channels))
+        w1 = weight_variable([filter_size, filter_size, channels, n_class], stddev)
+        b1 = bias_variable([n_class])
+        conv1 = conv2d(in_node, w1, keep_prob)
+        in_node = tf.nn.relu(conv1 + b1)
+        size -= 2
 
-        w2 = weight_variable(
-            [filter_size, filter_size, features, features], stddev)
-        b1 = bias_variable([features])
-        b2 = bias_variable([features])
+    weights = []
 
-        conv1 = conv2d(in_node, w1, keep_prob) # in_node as size [batch_size, nx, ny, channels]
-        tmp_h_conv = tf.nn.relu(conv1 + b1)
-        conv2 = conv2d(tmp_h_conv, w2, keep_prob)
-        dw_h_convs[layer], rnnweights, rnnbias = block_rnn_zeroinit(tf.nn.relu(conv2 + b2))
-        #weights.append(rnnweights)
-        #weights.append(rnnbias)
-
-        weights.append((w1, w2))
-        biases.append((b1, b2))
-        convs.append((conv1, conv2))
-
-        size -= 4
-        if layer < layers - 1:
-            pools[layer] = max_pool(dw_h_convs[layer], pool_size)
-            in_node = pools[layer]
-            size /= 2
-
-    in_node = dw_h_convs[layers - 1]
-
-    # up layers
-    for layer in range(layers - 2, -1, -1):
-        features = 2**(layer + 1) * features_root
-        stddev = np.sqrt(2 / (filter_size**2 * features))
-
-        wd = weight_variable_devonc(
-            [pool_size, pool_size, features // 2, features], stddev)
-        bd = bias_variable([features // 2])
-        h_deconv = tf.nn.relu(deconv2d(in_node, wd, pool_size) + bd)
-        h_deconv_concat = crop_and_concat(dw_h_convs[layer], h_deconv)
-        deconv[layer] = h_deconv_concat
-
-        w1 = weight_variable(
-            [filter_size, filter_size, features, features // 2], stddev)
-        w2 = weight_variable(
-            [filter_size, filter_size, features // 2, features // 2], stddev)
-        b1 = bias_variable([features // 2])
-        b2 = bias_variable([features // 2])
-
-        conv1 = conv2d(h_deconv_concat, w1, keep_prob)
-        h_conv = tf.nn.relu(conv1 + b1)
-        conv2 = conv2d(h_conv, w2, keep_prob)
-        in_node, rnnweights, rnnbias  = block_rnn_zeroinit(tf.nn.relu(conv2 + b2))
-        #weights.append(rnnweights)
-        #weights.append(rnnbias)
-        up_h_convs[layer] = in_node
-
-        weights.append((w1, w2))
-        biases.append((b1, b2))
-        convs.append((conv1, conv2))
-
-        size *= 2
-        size -= 4
-
-    # Output Map
-    weight = weight_variable([1, 1, features_root, n_class], stddev)
-    bias = bias_variable([n_class])
-    conv = conv2d(in_node, weight, tf.constant(1.0))
-    output_map = tf.nn.relu(conv + bias)
-    up_h_convs["out"] = output_map
-
-    if summaries:
-        for i, (c1, c2) in enumerate(convs):
-            tf.summary.image('summary_conv_%02d_01' % i, get_image_summary(c1))
-            tf.summary.image('summary_conv_%02d_02' % i, get_image_summary(c2))
-
-        for k in pools.keys():
-            tf.summary.image('summary_pool_%02d' %
-                             k, get_image_summary(pools[k]))
-
-        for k in deconv.keys():
-            tf.summary.image('summary_deconv_concat_%02d' %
-                             k, get_image_summary(deconv[k]))
-
-        for k in dw_h_convs.keys():
-            tf.summary.histogram("dw_convolution_%02d" %
-                                 k + '/activations', dw_h_convs[k])
-
-        for k in up_h_convs.keys():
-            tf.summary.histogram("up_convolution_%s" %
-                                 k + '/activations', up_h_convs[k])
-
-    variables = []
-    for w1, w2 in weights:
-        variables.append(w1)
-        variables.append(w2)
-
-    for b1, b2 in biases:
-        variables.append(b1)
-        variables.append(b2)
-
-    return output_map, variables, int(in_size - size)
+    return in_node, variables, int(in_size - size)
 
 
-class RUnet_nobatch(object):
+class RUnet_test(object):
     """
     A unet implementation
 
@@ -199,20 +101,19 @@ class RUnet_nobatch(object):
         self.n_class = n_class
         self.summaries = kwargs.get("summaries", True)
 
-        self.x = tf.placeholder("float", shape=[None, None, None, channels])
-        self.gt1 = tf.placeholder("float", shape=[1, None, None, n_class])
-        self.gt = tf.placeholder("float", shape=[None, None, None, n_class])
-        self.keep_prob = tf.placeholder(
-            tf.float32)  # dropout (keep probability)
+        self.x = tf.placeholder("float", shape=[None, None, None, None, channels])
+        #self.gt1 = tf.placeholder("float", shape=[1, None, None, n_class])
+        self.y = tf.placeholder("float", shape=[None, None, None, None, n_class])
+        self.keep_prob = tf.placeholder(tf.float32)  # dropout (keep probability)
 
-        logits, self.variables, self.offset = create_r_conv_net_nobatch(
-            self.x, self.gt1, self.keep_prob, channels, n_class, **kwargs)
+        logits, self.variables, self.offset = create_conv_r_net_test(
+            self.x , self.keep_prob, channels, n_class, **kwargs)
 
         self.cost = self._get_cost(logits, cost, cost_kwargs)
 
         self.gradients_node = tf.gradients(self.cost, self.variables)
 
-        self.cross_entropy = tf.reduce_mean(cross_entropy(tf.reshape(self.gt, [-1, n_class]),
+        self.cross_entropy = tf.reduce_mean(cross_entropy(tf.reshape(self.y, [-1, n_class]),
                                                           tf.reshape(pixel_wise_softmax_2(logits), [-1, n_class])))
 
         self.predicter = pixel_wise_softmax_2(logits)
