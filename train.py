56b0032f1067dd0bf290f6da0923e58e4e2a9ad6
cjl <cuijialiang@pku.edu.cn>
2019-03-12 19:33:04 +0800

    add hist

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 from meval import calc_auc, print_step_auc
+import text_histogram
 
 def train(model_path = None,
           save_path = '/home/cjl/tf_runet/models/20180612',
@@ -44,27 +45,14 @@ def train(model_path = None,
         print('total_step:', total_step)
         #iptdata, gtdata = data_provider.get_one_data_with_maxstep_next_batch(cfg.batch_size, cfg.max_step, max_size,model.offset)
         iptdata, gtdata = data_provider.get_a_random_batch()
-        summary, cost, otherlabels, predict = model.train(iptdata, gtdata, data_provider.get_mark)
+        summary, cost, otherlabels, predict = model.train(iptdata, gtdata, None)
         auc = calc_auc(predict, otherlabels)
         print("cost:", cost, " auc:" , auc)
         print_step_auc(predict, otherlabels)
+        text_histogram.histogram(list(predict.astype(float).reshape((-1))))
 
         train_writer.add_summary(summary, total_step)
-        if display and (total_step%displaystep) == 0:
-            otherlabels = otherlabels[0]
-            predict = predict[0]
-            lbimg = oneHot_to_gray255(otherlabels[0])
-            gtimg = oneHot_to_gray255(predict[0])
-            img = np.append(lbimg,gtimg, axis=0)
-            for step in range(1, 9 if 9 < max_step - 1 else max_step - 1):
-                nimg = np.append(oneHot_to_gray255(otherlabels[step]),oneHot_to_gray255(predict[step]), axis=0)
-                img = np.append(img, nimg, axis=1)
-            for proc in psutil.process_iter():
-                if proc.name() == "display":
-                    proc.kill()
-            Image.fromarray(img).show(title='0,5')
-        print('--------------------------------------')
-        if (save and total_step % 50 == 0):
+        if (save and total_step % 20 == 0):
             filename = save_path + '/train' + str(total_step)
             model.save(filename)
     print('========================================')
@@ -78,7 +66,7 @@ if __name__ == '__main__':
     scripts_path = os.path.split( os.path.realpath( sys.argv[0] ) )[0]
     train(
         pro_path = scripts_path,
-        model_path = '/home/cjl/tf_runet/models/0329_1/train100',
-        save_path = scripts_path + '/models/0329_2',
+        model_path = None,
+        save_path = scripts_path + '/models/0312_0',
         max_size = (300,300),
         dataidx = 10)
